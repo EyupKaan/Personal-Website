26cdf36d94edb41206e72644da2f169f80382d76
Eyüp Kaan Özteriş <68303307+EyupKaan@users.noreply.github.com>
2022-09-14 16:50:16 +0300

    add dist

---
+++ dist/server/components/about.js
@@ -0,0 +1,59 @@
+exports.ids = [1];
+exports.modules = {
+
+/***/ 37:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/about.vue?vue&type=template&id=5c866a4d&
+var render = function render() {
+  var _vm = this,
+      _c = _vm._self._c;
+
+  return _c('section', {
+    staticClass: "text-gray-900 border-b pb-16 bg-gray-300"
+  }, [_vm._ssrNode("<div class=\"container max-w-screen-lg min-h-screen mx-auto my-auto md:items-start md:text-left items-center py-32 pb-12 flex flex-col px-5\"><h1 class=\"text-left text-3xl font-medium title-font text-gray-900\">\n            About Me\n        </h1> <span class=\"block mt-1 w-10 h-1 bg-green-700\"></span></div>")]);
+};
+
+var staticRenderFns = [];
+
+// CONCATENATED MODULE: ./components/about.vue?vue&type=template&id=5c866a4d&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/about.vue?vue&type=script&lang=js&
+/* harmony default export */ var aboutvue_type_script_lang_js_ = ({});
+// CONCATENATED MODULE: ./components/about.vue?vue&type=script&lang=js&
+ /* harmony default export */ var components_aboutvue_type_script_lang_js_ = (aboutvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./components/about.vue
+
+
+
+function injectStyles (context) {
+  
+  
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  components_aboutvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  null,
+  "af7a2d04"
+  
+)
+
+/* harmony default export */ var about = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=about.js.map
---
+++ dist/server/pages/about.js
@@ -0,0 +1,123 @@
+exports.ids = [5,1];
+exports.modules = {
+
+/***/ 37:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/about.vue?vue&type=template&id=5c866a4d&
+var render = function render() {
+  var _vm = this,
+      _c = _vm._self._c;
+
+  return _c('section', {
+    staticClass: "text-gray-900 border-b pb-16 bg-gray-300"
+  }, [_vm._ssrNode("<div class=\"container max-w-screen-lg min-h-screen mx-auto my-auto md:items-start md:text-left items-center py-32 pb-12 flex flex-col px-5\"><h1 class=\"text-left text-3xl font-medium title-font text-gray-900\">\n            About Me\n        </h1> <span class=\"block mt-1 w-10 h-1 bg-green-700\"></span></div>")]);
+};
+
+var staticRenderFns = [];
+
+// CONCATENATED MODULE: ./components/about.vue?vue&type=template&id=5c866a4d&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/about.vue?vue&type=script&lang=js&
+/* harmony default export */ var aboutvue_type_script_lang_js_ = ({});
+// CONCATENATED MODULE: ./components/about.vue?vue&type=script&lang=js&
+ /* harmony default export */ var components_aboutvue_type_script_lang_js_ = (aboutvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./components/about.vue
+
+
+
+function injectStyles (context) {
+  
+  
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  components_aboutvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  null,
+  "af7a2d04"
+  
+)
+
+/* harmony default export */ var about = __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ 41:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/about.vue?vue&type=template&id=83b6527e&
+var render = function render() {
+  var _vm = this,
+      _c = _vm._self._c;
+
+  return _c('About');
+};
+
+var staticRenderFns = [];
+
+// CONCATENATED MODULE: ./pages/about.vue?vue&type=template&id=83b6527e&
+
+// EXTERNAL MODULE: ./components/about.vue + 4 modules
+var about = __webpack_require__(37);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/about.vue?vue&type=script&lang=js&
+
+/* harmony default export */ var aboutvue_type_script_lang_js_ = ({
+  components: {
+    About: about["default"]
+  }
+});
+// CONCATENATED MODULE: ./pages/about.vue?vue&type=script&lang=js&
+ /* harmony default export */ var pages_aboutvue_type_script_lang_js_ = (aboutvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./pages/about.vue
+
+
+
+function injectStyles (context) {
+  
+  
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pages_aboutvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  null,
+  "95d731e4"
+  
+)
+
+/* harmony default export */ var pages_about = __webpack_exports__["default"] = (component.exports);
+
+/* nuxt-component-imports */
+installComponents(component, {About: __webpack_require__(37).default})
+
+
+/***/ })
+
+};;
+//# sourceMappingURL=about.js.map
